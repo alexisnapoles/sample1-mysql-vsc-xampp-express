93d438a21946637d9e46a5b323630f9fa097ba95
alexisnapoles <alexisndelatorre@gmail.com>
2022-02-19 09:22:11 +0800

    added base layout

--- routes/landing.js
+++ routes/landing.js
@@ -4,7 +4,13 @@ const router = express.Router();
 
 // add a new rout to the express router
 router.get('/', (req, res) => {
-    res.send('welcome')
-})
+    res.render('landing/index')
+});
+router.get('/about-us', (req, res) => {
+    res.render('landing/about-us')
+});
+router.get('/contact-us', (req, res) => {
+    res.render('landing/contact-us')
+});
 
 module.exports = router;
