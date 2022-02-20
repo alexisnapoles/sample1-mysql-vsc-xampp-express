0c67274b194d117fe9a275645046bfa0c3a314e4
alexisnapoles <alexisndelatorre@gmail.com>
2022-02-20 09:48:07 +0800

    included database information in .env file

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 
 // Create connection mysql
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodemysql'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 // connect
